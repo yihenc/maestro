60a541ba23a3ae0dccd883ac4f5cbc42fa25ba67
yihenc <48312380+yihenc@users.noreply.github.com>
2023-11-30 10:40:24 -0800

    Add files via upload

---
+++ run_vgg16.sh
@@ -0,0 +1,5 @@
+./maestro --HW_file='data/hw/my_accelerator.m' \
+          --Mapping_file='data/mapping/vgg16_dataflow.m' \
+          --print_res=true \
+          --print_res_csv_file=true \
+          --print_log_file=false \
