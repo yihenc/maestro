d61ac9bbe7951e61fddbe69e477e45bbf60910de
yihenc <48312380+yihenc@users.noreply.github.com>
2023-12-01 13:53:10 -0800

    Update run_example.sh

--- run_example.sh
+++ run_example.sh
@@ -1,5 +1,6 @@
-./maestro --HW_file='data/hw/accelerator_1.m' \
-          --Mapping_file='data/mapping/Resnet50_kcp_ws.m' \
+./maestro --HW_file='data/hw/my_accelerator.m' \
+          --Mapping_file='data/mapping/vgg16_dataflow.m' \
           --print_res=true \
           --print_res_csv_file=true \
           --print_log_file=false \
+
